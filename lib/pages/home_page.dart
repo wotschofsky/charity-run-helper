5c1d41fb0bbebd7a6602158d8b242817197a7504
Felix Wotschofsky <hello@felisk.io>
2021-05-31 00:18:59 +0200

    Customize and extract home page 🏡

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Charity Run Helper'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('Welcome!', style: TextStyle(fontSize: 48)),
+              Padding(
+                padding: const EdgeInsets.only(top: 32),
+                child: Text(
+                    'Get started by creating an event or signing up for one.',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: Colors.grey, fontSize: 20)),
+              )
+            ],
+          ),
+        ));
+  }
+}
