e28db9514f15d8959d7d6a8527912431efe33334
Felix Wotschofsky <hello@felisk.io>
2021-06-10 23:31:46 +0200

    Add clean up Cloud Functions 🚮

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,68 @@
-import * as functions from "firebase-functions";
+import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const DEFAULT_REGION = 'europe-west3';
+
+admin.initializeApp();
+const db = admin.firestore();
+
+const eventsCollection = db.collection('events');
+const participationsCollection = db.collection('participations');
+const sponsorsCollection = db.collection('sponsors');
+
+export const usersCleanUp = functions
+  .region(DEFAULT_REGION)
+  .auth.user()
+  .onDelete(async (user) => {
+    const { uid } = user;
+
+    // Clean up events
+    const eventsQuery = await eventsCollection
+      .where('createdBy', '==', uid)
+      .get();
+
+    eventsQuery.forEach((doc) => {
+      doc.ref.delete();
+    });
+
+    // Clean up participations
+    const participationsQuery = await participationsCollection
+      .where('runnerId', '==', uid)
+      .get();
+
+    participationsQuery.forEach((doc) => {
+      doc.ref.delete();
+    });
+  });
+
+export const eventsCleanUp = functions
+  .region(DEFAULT_REGION)
+  .firestore.document('events/{eventId}')
+  .onDelete(async (snapshot, context) => {
+    const { eventId } = context.params;
+
+    // Clean up participations
+    const query = await participationsCollection
+      .where('eventId', '==', eventId)
+      .get();
+
+    query.forEach((doc) => {
+      doc.ref.delete();
+    });
+  });
+
+export const participationsCleanUp = functions
+  .region(DEFAULT_REGION)
+  .firestore.document('participations/{participationId}')
+  .onDelete(async (snapshot, context) => {
+    const { participationId } = context.params;
+
+    // Clean up sponsors
+    const query = await sponsorsCollection
+      .where('participationId', '==', participationId)
+      .get();
+
+    query.forEach((doc) => {
+      doc.ref.delete();
+    });
+  });
